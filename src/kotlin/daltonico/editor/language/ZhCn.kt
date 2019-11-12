ddd26bc36cd92fa043cf8692aa6a63ffb93aedf7
Sun <sunxingyun@gmail.com>
2019-11-12 11:38:56 -0300

    Add Chinese language

--- src/kotlin/daltonico/editor/language/ZhCn.kt
+++ src/kotlin/daltonico/editor/language/ZhCn.kt
@@ -2,18 +2,18 @@ package daltonico.editor.language
 
 class ZhCn : Language() {
     override val dictionary = mapOf(
-        "title" to "Editor Daltônico",
-        "file" to "Arquivo",
-        "image" to "Imagem",
-        "options" to "Opções",
-        "open_file" to "Abrir",
-        "save_file" to "Salvar",
-        "gray_scale" to "Escala de cinza",
-        "black_white" to "Preto e branco",
-        "filters" to "Filtros",
-        "filter_1" to "Filtro 1",
-        "filter_2" to "Filtro 2",
-        "histogram" to "Histograma",
-        "languages" to "Idiomas"
+        "title" to "色盲编辑器",
+        "file" to "文件",
+        "image" to "图片",
+        "options" to "选择",
+        "open_file" to "打开",
+        "save_file" to "储存",
+        "gray_scale" to "灰度",
+        "black_white" to "黑白",
+        "filters" to "过滤",
+        "filter_1" to "过滤 1",
+        "filter_2" to "过滤 2",
+        "histogram" to "直方图",
+        "languages" to "语言"
     )
 }
