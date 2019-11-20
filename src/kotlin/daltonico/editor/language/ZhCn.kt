18c314e5352cb9ea8c959522ef141c6445f8821c
Sun <sunxingyun@gmail.com>
2019-11-20 13:55:10 -0300

    Update ZhCn.kt

--- src/kotlin/daltonico/editor/language/ZhCn.kt
+++ src/kotlin/daltonico/editor/language/ZhCn.kt
@@ -8,27 +8,17 @@ class ZhCn : Language() {
         "options" to "选择",
         "open_file" to "打开",
         "save_file" to "储存",
-<<<<<<< Updated upstream
-        "gray_scale" to "灰度",
-        "black_white" to "黑白",
-        "filters" to "过滤",
-        "filter_1" to "过滤 1",
-        "filter_2" to "过滤 2",
-        "histogram" to "直方图",
-        "languages" to "语言"
-=======
         "close_file" to "关闭",
         "gray_scale" to "灰度",
         "black_white" to "黑白",
         "filters" to "过滤",
-        "filter_1" to "投影机", // Sun, traduz pra nós por favor :)
+        "filter_1" to "聚光灯", // Sun, traduz pra nós por favor :)
         "filter_2" to "反转",
         "histogram" to "直方图",
         "languages" to "语言",
-        "save_changes" to "想要保存你的改动吗?",
+        "save_changes" to "您要保存更改吗?",
         "save" to "保存",
         "dont_save" to "不保存",
         "cancel" to "取消"
->>>>>>> Stashed changes
     )
 }
